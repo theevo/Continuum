4e82295bd5394aca00143c6b2f54cfc453e8d321
Theo Vora <theo.vora@gmail.com>
2020-03-31 16:09:18 -0600

    add model controller

---
+++ Continuum/Controllers/Helpers/PostError.swift
@@ -0,0 +1,33 @@
+//
+//  PostError.swift
+//  Continuum
+//
+//  Created by theevo on 3/31/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import Foundation
+
+enum PostError: LocalizedError {
+    case thrown(Error)
+    case invalidURL
+    case noData
+    case badData
+    case failToDelete
+    
+    var errorDescription: String? {
+        switch self {
+            
+        case .thrown(let error):
+            return error.localizedDescription
+        case .invalidURL:
+            return "Unable to reach server."
+        case .noData:
+            return "Server responded with no data."
+        case .badData:
+            return "Server returned bad data."
+        case .failToDelete:
+            return "Delete failed."
+        }
+    } // end errorDescription
+} // end enum
---
+++ Continuum/Controllers/Model Controller/PostController.swift
@@ -0,0 +1,32 @@
+//
+//  PostController.swift
+//  Continuum
+//
+//  Created by theevo on 3/31/20.
+//  Copyright © 2020 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    // MARK: - Singleton and Source of Truth
+    static let shared = PostController()
+    var posts: [Post] = []
+    
+    
+    
+    // MARK: - CRUD Functions
+    
+    func addComment(text: String, to post: Post, completion: @escaping (Result<Comment, PostError>) -> Void) {
+        let comment = Comment(text: text, post: post)
+        post.comments.append(comment)
+    }
+    
+    func createPostWith(image: UIImage, caption: String, completion: @escaping (Result<Post?, PostError>) -> Void) {
+        let post = Post(photo: image, caption: caption)
+        posts.append(post)
+    }
+    
+    
+} // end class
